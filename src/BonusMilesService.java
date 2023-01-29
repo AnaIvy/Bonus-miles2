3b1d6b3adcdf74b49f40c9942c46b874cf780ff5
Papillon <anil_061293@gmail.com>
2023-01-29 18:37:09 +0300

    Commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,7 @@
 public class BonusMilesService {
-    int miles;
     public int calculate(int price) {
-        miles =  price/ 20;
+        int miles;
+        miles = price / 20;
         return miles;
     }
 }
